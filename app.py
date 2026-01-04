230099a90d2a3108a8c62cb57742aed4aa889135
Husnain Laptop <abbasanam197@gmail.com>
2026-01-04 18:59:29 +0500

    Prepare backend for Railway deployment

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
+# Test route
 @app.route('/')
 def home():
     return "Hello, your Python backend is working!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Route to handle order
+@app.route('/place-order', methods=['POST'])
+def place_order():
+    data = request.get_json()
+    print("Order received:", data)
+    return jsonify({"message": "Order received!"})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
